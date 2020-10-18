f21ab19a39e3402615e98102a7bfd2c311eb447f
Axel <axel.bie@metronlab.com>
2020-10-18 17:25:22 +0200

    ci(): needs error

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,7 +3,7 @@ import { createServer, IncomingMessage, ServerResponse } from 'http';
 const port = 5002;
 
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
-    response.end('Hello JeanJean');
+  response.end('Hello JeanJean');
 });
 
 server.listen(port);
